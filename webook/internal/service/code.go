b7484fba5fa2c86161f5a08eab5ca78d46ca8ba4
zmsocc <zmsocc@163.com>
2025-03-21 18:46:44 +0800

    3.21-验证码的发送验证

---
+++ webook/internal/service/code.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"gitee.com/zmsoc/gogogo/webook/internal/repository"
+	"gitee.com/zmsoc/gogogo/webook/internal/service/sms"
+	"math/rand"
+)
+
+const codeTplId = "1877556"
+
+type CodeService struct {
+	repo   *repository.CodeRepository
+	smsSvc sms.Service
+}
+
+// biz 区别业务场景
+func (svc *CodeService) Send(ctx context.Context, biz string, phone string) error {
+	// 生成一个验证码
+	code := svc.generateCode()
+	// 塞进去 Redis
+	err := svc.repo.Store(ctx, biz, phone, code)
+	if err != nil {
+		// 有问题
+		return err
+	}
+	// 发送出去
+	err = svc.smsSvc.Send(ctx, codeTplId, []string{code}, phone)
+	return err
+}
+
+func (svc *CodeService) Verify(ctx context.Context, biz string, phone string, inputCode string) (bool, error) {
+	return svc.repo.Verify(ctx, biz, phone, inputCode)
+}
+
+func (svc *CodeService) generateCode() string {
+	// 六位数， num 在0， 1000000之间，包含0，不包含1000000
+	num := rand.Intn(1000000)
+	// 不够六位的，前面加上0，补够六位
+	// 000001
+	return fmt.Sprintf("%6d", num)
+}
